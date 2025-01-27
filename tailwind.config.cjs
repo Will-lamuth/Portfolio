e872950662dd974720b941aa8b33a9491a3fd2c2
Will-lamuth <156252007+Will-lamuth@users.noreply.github.com>
2025-01-27 11:39:14 -0500

    Code Cleanup + font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				csutom:['"SF UI Text"', '"Helvetica Neue"', '"Segoe UI"', 'Oxygen', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
